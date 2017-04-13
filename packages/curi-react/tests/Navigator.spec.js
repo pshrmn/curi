1104064f44150be79bc12f549ce1a1454dfc1642
Paul Sherman <paul.sherman.88@gmail.com>
2017-04-13 14:22:50 -0500

    Improve <Navigator> test coverage
    Still need to actually write tests for the other code

--- packages/curi-react/tests/Navigator.spec.js
+++ packages/curi-react/tests/Navigator.spec.js
@@ -20,26 +20,33 @@ describe('<Navigator>', () => {
     );
     expect(fn.mock.calls.length).toBe(1);
   });
-
+  
   it('passes the children function a response object', (done) => {
     const history = createMemoryHistory();
     const routes = [
       { name: 'Home', path: path('', { end: true })},
       { name: 'About', path: path('about') }
     ];
-    const fn = jest.fn((resp) => {
-      expect(resp).toBeInstanceOf(Response);
+    let receivedResponse;
+    const fn = jest.fn((response) => {
+      receivedResponse = response;
       return null;
     });
 
     const config = createConfig(history, routes)
     config.ready()
-      .then(() => {
+      .then((resp) => {
         const wrapper = shallow(
           <Navigator config={config}>
             {fn}
           </Navigator>
         );
+
+        const properties = ['location', 'status', 'name', 'partials', 'params', 'body'];
+        expect(Object.keys(receivedResponse).length).toEqual(properties.length);
+        properties.forEach(key => {
+          expect(receivedResponse.hasOwnProperty(key)).toBe(true);
+        });
         done();
       });
   });
@@ -54,11 +61,10 @@ describe('<Navigator>', () => {
     let first = true;
     const fn = jest.fn((resp) => {
       if (first) {
-        expect(resp.uri.name).toBe('Home');
+        expect(resp.name).toBe('Home');
         first = false;
       } else {
-        expect(resp.uri.name).toBe('About');
-        done();
+        expect(resp.name).toBe('About');
       }
       return null;
     });
@@ -72,29 +78,61 @@ describe('<Navigator>', () => {
           </Navigator>
         );
         history.push('/about');
+        done();
       });
   });
 
   describe('response', () => {
     it('renders using response if provided', () => {
-
-    });
-
-    it('does not call subscribe when passed a resposne', () => {
       const fakeConfig = {
         subscribe: jest.fn()
       };
       const resp = new Response({ pathname: '/testing' });      
+      const respObj = resp.asObject();
       let received;
       const children = jest.fn((response) => {
         received = response;
         return null;
       });
       const wrapper = shallow(
-        <Navigator response={resp} config={fakeConfig} children={children} />
+        <Navigator response={respObj} config={fakeConfig} children={children} />
+      );
+      expect(received).toBe(respObj);
+    });
+
+    it('does not call subscribe when passed a response', () => {
+      const fakeConfig = {
+        subscribe: jest.fn()
+      };
+      const resp = new Response({ pathname: '/testing' });      
+      const respObj = resp.asObject();
+      const wrapper = shallow(
+        <Navigator response={respObj} config={fakeConfig} children={() => null} />
       );
       expect(fakeConfig.subscribe.mock.calls.length).toBe(0);
-      expect(received).toBe(resp);
+    });
+  });
+
+  describe('unmount', () => {
+    it('unsubscribes from the config', () => {
+      const unsub = jest.fn();
+      const sub = jest.fn((fn) => {
+        return unsub;
+      });
+      const fakeConfig = {
+        subscribe: sub
+      };
+
+      expect(unsub.mock.calls.length).toBe(0);
+      expect(sub.mock.calls.length).toBe(0);
+
+      const wrapper = shallow(
+        <Navigator config={fakeConfig} children={() => null} />
+      );
+      expect(sub.mock.calls.length).toBe(1);
+      expect(unsub.mock.calls.length).toBe(0);
+      wrapper.unmount()
+      expect(unsub.mock.calls.length).toBe(1);
     });
   });
 });
