ee22ddccd5b6faddfafbf5b53f15f6c29fd2af4b
Paul Sherman <paul.sherman.88@gmail.com>
2017-04-25 01:35:46 -0500

    Add curi-react-link package

---
+++ packages/curi-react-link/src/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import invariant from 'invariant';
+
+const canNavigate = event => {
+  return (
+    !event.defaultPrevented &&
+    event.button === 0 &&
+    !(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
+  );
+};
+
+class Link extends Component {
+  static propTypes = {
+    name: PropTypes.string,
+    params: PropTypes.object,
+    to: PropTypes.object,
+    onClick: PropTypes.func
+  };
+
+  static contextTypes = {
+    curi: PropTypes.object.isRequired
+  };
+
+  clickHandler = event => {
+    if (this.props.onClick) {
+      this.props.onClick(event);
+    }
+
+    if (canNavigate(event) && !this.props.target) {
+      event.preventDefault();
+      const { curi } = this.context;
+      const { pathname } = this.state;
+      const {
+        name,
+        params,
+        to = {}
+      } = this.props;
+      const location = { pathname, ...to };
+      curi.history.push(location);
+    }
+  };
+
+  createPathname(props, context) {
+    const { name, params } = props;
+    const { curi } = context;
+    const pathname = name != null ? curi.addons.pathname(name, params) : '/';
+    this.setState(() => ({
+      pathname
+    }));
+  }
+
+  componentWillMount() {
+    this.createPathname(this.props, this.context);
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    this.createPathname(nextProps, nextContext);
+  }
+
+  render() {
+    const { name, params, to, onClick, ...rest } = this.props;
+    const { curi } = this.context;
+    const { pathname } = this.state;
+    const href = curi.history.createHref({ pathname, ...to });
+    return <a onClick={this.clickHandler} href={href} {...rest} />;
+  }
+}
+
+export default Link;
---
+++ packages/curi-react-link/tests/Link.spec.js
@@ -0,0 +1,241 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { createMemoryHistory } from 'history';
+import createConfig from '../../curi/src';
+import Link from '../src';
+
+describe('<Link>', () => {
+  it('errors if it cannot access a curi config', () => {
+    const err = console.error;
+    console.error = () => {};
+
+    expect(() => {
+      shallow(<Link name="Test">Test</Link>);
+    }).toThrow();
+
+    console.error = err;
+  });
+
+  it('renders an <a>', () => {
+    const history = createMemoryHistory();
+    const config = createConfig(history, [{ name: 'Test', path: '' }]);
+    const wrapper = shallow(<Link name="Test">Test</Link>, {
+      context: { curi: config }
+    });
+    const a = wrapper.find('a');
+    expect(a.exists()).toBe(true);
+  });
+
+
+  describe('name', () => {
+    it('sets the href attribute using the named route\'s path', () => {
+      const history = createMemoryHistory();
+      const config = createConfig(history, [{ name: 'Test', path: '' }]);
+      const wrapper = shallow(<Link name="Test">Test</Link>, {
+        context: { curi: config }
+      });
+      const a = wrapper.find('a');
+      expect(a.prop('href')).toBe('/');
+    });
+
+    it('defaults to pathname="/" if name is not provided', () => {
+      const history = createMemoryHistory();
+      const config = createConfig(history, []);
+      const wrapper = shallow(<Link>Test</Link>, {
+        context: { curi: config }
+      });
+      const a = wrapper.find('a');
+      expect(a.prop('href')).toBe('/');
+    });
+  });
+
+  describe('params', () => {
+    it('uses params to generate the href', () => {
+      const history = createMemoryHistory();
+      const config = createConfig(history, [
+        { name: 'Park', path: '/park/:name' }
+      ]);
+      const params = { name: 'Glacier' };
+      const wrapper = shallow(<Link name="Park" params={params}>Test</Link>, {
+        context: { curi: config }
+      });
+      const a = wrapper.find('a');
+      expect(a.prop('href')).toBe('/park/Glacier');
+    });
+
+    it('updates href when props change', () => {
+      const history = createMemoryHistory();
+      const config = createConfig(history, [
+        { name: 'Park', path: '/park/:name' }
+      ]);
+      const params = { name: 'Glacier' };
+      const wrapper = shallow(<Link name="Park" params={params}>Test</Link>, {
+        context: { curi: config }
+      });
+      let a = wrapper.find('a');
+      expect(a.prop('href')).toBe('/park/Glacier');
+
+      wrapper.setProps({ params: { name: 'Yellowstone' } });
+      a = wrapper.find('a');
+      expect(a.prop('href')).toBe('/park/Yellowstone');
+    });
+  });
+
+  describe('to', () => {
+    it('merges the to prop with the generated pathname when navigating', () => {
+      const history = createMemoryHistory();
+      const config = createConfig(history, [
+        { name: 'Test', path: 'test' }
+      ]);
+      const wrapper = shallow(
+        <Link name="Test" to={{ search: '?one=two', hash: '#hashtag' }}>
+          Test
+        </Link>,
+        { context: { curi: config } }
+      );
+      const a = wrapper.find('a');
+      expect(a.prop('href')).toBe('/test?one=two#hashtag');
+    });
+
+    it('overwrites the generated pathname if to includes one', () => {
+      const history = createMemoryHistory();
+      const config = createConfig(history, [
+        { name: 'Test', path: 'test' }
+      ]);
+      const wrapper = shallow(
+        <Link name="Test" to={{ pathname: '/not-a-test' }}>Test</Link>,
+        { context: { curi: config } }
+      );
+      const a = wrapper.find('a');
+      expect(a.prop('href')).toBe('/not-a-test');
+    });
+  });
+
+  describe('clicking a link', () => {
+    it('calls history.push', () => {
+      const history = createMemoryHistory();
+      history.push = jest.fn();
+
+      const config = createConfig(history, [{ name: 'Test', path: '' }]);
+      const wrapper = shallow(<Link name="Test">Test</Link>, {
+        context: { curi: config }
+      });
+      const leftClickEvent = {
+        defaultPrevented: false,
+        preventDefault() {
+          this.defaultPrevented = true;
+        },
+        metaKey: null,
+        altKey: null,
+        ctrlKey: null,
+        shiftKey: null,
+        button: 0
+      };
+      wrapper.find('a').simulate('click', leftClickEvent);
+      expect(history.push.mock.calls.length).toBe(1);
+    });
+
+    describe('onClick', () => {
+      it('calls onClick prop func if provided', () => {
+        const history = createMemoryHistory();
+        history.push = jest.fn();
+        const onClick = jest.fn();
+        const config = createConfig(history, [{ name: 'Test', path: '' }]);
+        const wrapper = shallow(<Link name="Test" onClick={onClick}>Test</Link>, {
+          context: { curi: config }
+        });
+        const leftClickEvent = {
+          defaultPrevented: false,
+          preventDefault() {
+            this.defaultPrevented = true;
+          },
+          metaKey: null,
+          altKey: null,
+          ctrlKey: null,
+          shiftKey: null,
+          button: 0
+        };
+        wrapper.find('a').simulate('click', leftClickEvent);
+        expect(onClick.mock.calls.length).toBe(1);
+        expect(history.push.mock.calls.length).toBe(1);
+      });
+
+      it('does not call history.push if onClick prevents default', () => {
+        const history = createMemoryHistory();
+        history.push = jest.fn();
+        const onClick = jest.fn(event => {
+          event.preventDefault();
+        });
+        const config = createConfig(history, [{ name: 'Test', path: '' }]);
+        const wrapper = shallow(<Link name="Test" onClick={onClick}>Test</Link>, {
+          context: { curi: config }
+        });
+        const leftClickEvent = {
+          defaultPrevented: false,
+          preventDefault() {
+            this.defaultPrevented = true;
+          },
+          metaKey: null,
+          altKey: null,
+          ctrlKey: null,
+          shiftKey: null,
+          button: 0
+        };
+        wrapper.find('a').simulate('click', leftClickEvent);
+        expect(onClick.mock.calls.length).toBe(1);
+        expect(history.push.mock.calls.length).toBe(0);
+      });
+    });
+
+    it("doesn't push for modified clicks", () => {
+      const history = createMemoryHistory();
+      history.push = jest.fn();
+
+      const config = createConfig(history, [{ name: 'Test', path: '' }]);
+      const wrapper = shallow(<Link name="Test">Test</Link>, {
+        context: { curi: config }
+      });
+      const modifiedClickEvent = {
+        defaultPrevented: false,
+        preventDefault() {
+          this.defaultPrevented = true;
+        },
+        metaKey: null,
+        altKey: null,
+        ctrlKey: null,
+        shiftKey: null,
+        button: 0
+      };
+      const modifiers = ['metaKey', 'altKey', 'ctrlKey', 'shiftKey'];
+      modifiers.forEach(m => {
+        const eventCopy = Object.assign({}, modifiedClickEvent);
+        eventCopy[m] = true;
+        wrapper.find('a').simulate('click', eventCopy);
+        expect(history.push.mock.calls.length).toBe(0);
+      });
+    });
+
+    it("doesn't push if event.preventDefault has been called", () => {
+      const history = createMemoryHistory();
+      history.push = jest.fn();
+
+      const config = createConfig(history, [{ name: 'Test', path: '' }]);
+      const wrapper = shallow(<Link name="Test">Test</Link>, {
+        context: { curi: config }
+      });
+      const preventedEvent = {
+        defaultPrevented: true,
+        preventDefault() {
+          this.defaultPrevented = true;
+        },
+        metaKey: null,
+        altKey: null,
+        ctrlKey: null,
+        shiftKey: null,
+        button: 0
+      };
+      wrapper.find('a').simulate('click', preventedEvent);
+      expect(history.push.mock.calls.length).toBe(0);
+    });
+  });
+});
