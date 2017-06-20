2a1ef747424012f7c3655e363ba49eb38c11bf5e
Paul Sherman <paul.sherman.88@gmail.com>
2017-06-20 13:30:29 -0500

    Switch curi-react-active to all rollup build

---
+++ packages/curi-react-active/babel/config.js
@@ -0,0 +1,23 @@
+const building = process.env.BABEL_ENV === 'build';
+
+const plugins = [
+  'transform-class-properties',
+  'transform-object-rest-spread'
+];
+
+if (building) {
+  plugins.push('external-helpers');
+}
+
+module.exports = {
+  presets: [
+    ['env', {
+      modules: building ? false : 'commonjs',
+      targets: {
+        browsers: ['> 1%']
+      }
+    }],
+    'react'
+  ],
+  plugins: plugins
+};
