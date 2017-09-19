42b3d57d5f10056d8d7cdfdde439c8de96b38ca7
Paul Sherman <paul.sherman.88@gmail.com>
2017-09-19 00:06:17 -0500

    (docs) Update package versions [ci skip]

--- documentation-website/src/client/constants/versions.js
+++ documentation-website/src/client/constants/versions.js
@@ -8,6 +8,6 @@ export default {
   "redux": "1.0.0-alpha.0",
   "side-effect-scroll": "1.0.0-beta.3",
   "side-effect-title": "1.0.0-beta.3",
-  "svelte": "1.0.0-alpha.2",
-  "vue": "1.0.0-beta.3"
+  "svelte": "1.0.0-alpha.3",
+  "vue": "1.0.0-beta.4"
 };
