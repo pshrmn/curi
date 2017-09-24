6bea65a8b0fb60b47dd4e1de826d762d71081edd
Paul Sherman <paul.sherman.88@gmail.com>
2017-09-23 22:42:36 -0500

    Don't hoist ts-jest, use jest.config.js files

---
+++ packages/addons/addon-active/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "moduleFileExtensions": [
+    "ts",
+    "js"
+  ],
+  "testMatch": [
+    "**/tests/**/*.spec.ts"
+  ],
+  "transform": {
+    "\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+  },
+  "globals": {
+    "ts-jest": {
+      "module": "es6"
+    }
+  }
+};
