648c4fb1e9ecec91981a901ce2e5af71a20b13b7
Paul Sherman <paul.sherman.88@gmail.com>
2017-09-29 12:51:29 -0500

    Updated @curi/core path fn type

--- packages/core/types/utils/createPath.d.ts
+++ packages/core/types/utils/createPath.d.ts
@@ -1,7 +1,6 @@
-import PathToRegexp, { Key } from 'path-to-regexp';
+import { RegExpOptions, Key } from 'path-to-regexp';
 export interface Path {
     re: RegExp;
     keys: Array<Key>;
 }
-declare const path: (pathString: string, options?: PathToRegexp.RegExpOptions) => Path;
-export default path;
+export default function path(pathString: string, options?: RegExpOptions): Path;
