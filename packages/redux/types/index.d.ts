cafdd06d79761e86a500997fe6a471ecceba09c4
Paul Sherman <paul.sherman.88@gmail.com>
2017-10-11 19:33:38 -0500

    Update @curi/redux types

--- packages/redux/types/index.d.ts
+++ packages/redux/types/index.d.ts
@@ -1,8 +1,13 @@
 import { CuriConfig, AnyResponse } from '@curi/core';
 import { Store, Action } from 'redux';
 export declare const LOCATION_CHANGE = "@@curi/LOCATION_CHANGE";
+export declare const ADD_CURI = "@@curi/ADD_CURI";
 export interface ResponseAction extends Action {
     response: AnyResponse;
 }
+export interface CuriAction extends Action {
+    curi: CuriConfig;
+}
 export declare const responseReducer: (state: AnyResponse, action: Action) => AnyResponse;
-export declare const syncResponses: (store: Store<any>, config: CuriConfig) => void;
+export declare const curiReducer: (state: CuriConfig, action: Action) => CuriConfig;
+export declare const syncResponses: (store: Store<any>, curi: CuriConfig) => void;
