c997d47460a15068aa509f6fd48cbe30698cf03f
Paul Sherman <paul.sherman.88@gmail.com>
2017-10-27 01:00:31 -0500

    (docs) Add tutorials to build [ci skip]

--- documentation-website/scripts/build.js
+++ documentation-website/scripts/build.js
@@ -8,10 +8,12 @@ const routes = require('../src/client/routes').default;
 const packages = require('../src/client/constants/packages').default;
 const guides = require('../src/client/constants/guides').default;
 const examples = require('../src/client/constants/examples').default;
+const tutorialsByName = require('../src/client/constants/tutorials').byName;
 
 const packageNames = packages.map(p => ({ package: p.name }));
 const guideNames = guides.map(p => ({ slug: p.slug }));
 const exampleNames = examples.map(p => ({ slug: p.slug }));
+const tutorialNames = Object.keys(tutorialsByName).map(name => ({ name }));
 
 updatePackageVersions();
 
@@ -37,6 +39,9 @@ server = app.listen('8000', () => {
             params: exampleNames
           }
         }
+      },
+      'Tutorial': {
+        params: tutorialNames
       }
     }
   )
