d771627ef31a6e63258dc2e405ca2ac7d2d594ff
Paul Sherman <paul.sherman.88@gmail.com>
2017-11-28 01:15:32 -0600

    (examples) Render null before redux dispatch [ci skip]

--- examples/redux/src/renderFunction.js
+++ examples/redux/src/renderFunction.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Nav from './components/Nav';
 
 export default function(response) {
+  if (!response) {
+    return null;
+  }
   const { params, body:Body } = response;
   return (
     <div>
