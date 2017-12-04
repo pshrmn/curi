c914f86ee58c79ed6b3652b1c8385d47806e179c
Paul Sherman <paul.sherman.88@gmail.com>
2017-12-04 01:48:56 -0600

    (docs) Update pkg versions [ci skip]

--- documentation-website/src/client/constants/versions.js
+++ documentation-website/src/client/constants/versions.js
@@ -3,11 +3,11 @@ export default {
   "addon-active": "1.0.0-beta.5",
   "addon-ancestors": "1.0.0-beta.5",
   "addon-prefetch": "1.0.0-beta.5",
-  "core": "1.0.0-beta.18",
+  "core": "1.0.0-beta.19",
   "react": "1.0.0-beta.13",
-  "redux": "1.0.0-beta.0",
+  "redux": "1.0.0-beta.1",
   "side-effect-scroll": "1.0.0-beta.5",
   "side-effect-title": "1.0.0-beta.5",
   "svelte": "1.0.0-beta.1",
-  "vue": "1.0.0-beta.8"
+  "vue": "1.0.0-beta.10"
 };
