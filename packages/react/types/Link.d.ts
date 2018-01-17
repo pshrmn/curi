3d9cdd561ac6c60ac97978b592e321fdebbe42fb
Paul Sherman <paul.sherman.88@gmail.com>
2018-01-17 00:43:04 -0600

    Update dev deps
    Rollup, Jest

--- packages/react/types/Link.d.ts
+++ packages/react/types/Link.d.ts
@@ -1,37 +1,41 @@
 /// <reference types="react" />
-import React from 'react';
-import PropTypes from 'prop-types';
-import { CuriContext } from './interface';
-import { CuriRouter, Response } from '@curi/core';
-import { HickoryLocation } from '@hickory/root';
+import React from "react";
+import PropTypes from "prop-types";
+import { CuriContext } from "./interface";
+import { CuriRouter, Response } from "@curi/core";
+import { HickoryLocation } from "@hickory/root";
 export interface ActiveLink {
-    merge(props: object): object;
-    partial?: boolean;
-    extra?(l: HickoryLocation, d: object): boolean;
+  merge(props: object): object;
+  partial?: boolean;
+  extra?(l: HickoryLocation, d: object): boolean;
 }
-export interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-    to: string;
-    params?: object;
-    details?: object;
-    onClick?: (e: React.MouseEvent<HTMLElement>) => void;
-    active?: ActiveLink;
-    anchor?: React.ReactType;
-    target?: string;
-    router?: CuriRouter;
-    response?: Response;
+export interface LinkProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  to: string;
+  params?: object;
+  details?: object;
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+  active?: ActiveLink;
+  anchor?: React.ReactType;
+  target?: string;
+  router?: CuriRouter;
+  response?: Response;
 }
 export interface LinkState {
-    pathname: string;
+  pathname: string;
 }
 declare class Link extends React.Component<LinkProps, LinkState> {
-    static contextTypes: {
-        curi: PropTypes.Requireable<any>;
-    };
-    clickHandler: (event: React.MouseEvent<HTMLElement>) => void;
-    createPathname(props: LinkProps, context: CuriContext): void;
-    componentWillMount(): void;
-    componentWillReceiveProps(nextProps: LinkProps, nextContext: CuriContext): void;
-    verifyActiveAddon(): void;
-    render(): React.ReactElement<any>;
+  static contextTypes: {
+    curi: PropTypes.Requireable<any>;
+  };
+  clickHandler: (event: React.MouseEvent<HTMLElement>) => void;
+  createPathname(props: LinkProps, context: CuriContext): void;
+  componentWillMount(): void;
+  componentWillReceiveProps(
+    nextProps: LinkProps,
+    nextContext: CuriContext
+  ): void;
+  verifyActiveAddon(): void;
+  render(): React.ReactElement<any>;
 }
 export default Link;
