bbc63765bfbc5dfc370d27230c5188be11a4de32
Paul Sherman <paul.sherman.88@gmail.com>
2018-01-20 21:28:53 -0600

    (docs) MobX + React example [ci skip]

--- documentation-website/src/client/Examples/index.js
+++ documentation-website/src/client/Examples/index.js
@@ -1,47 +1,49 @@
 // misc
-import CodeSplitting from './misc/CodeSplitting';
-import ScriptTags from './misc/ScriptTags';
-import ServerRendering from './misc/ServerRendering';
-import SideEffect from './misc/SideEffect';
+import CodeSplitting from "./misc/CodeSplitting";
+import ScriptTags from "./misc/ScriptTags";
+import ServerRendering from "./misc/ServerRendering";
+import SideEffect from "./misc/SideEffect";
 // react
-import ActiveLinksReact from './react/ActiveLinks';
-import AuthenticationReact from './react/Authentication';
-import BasicReact from './react/Basic';
-import BlockingNavigationReact from './react/BlockingNavigation';
-import BreadcrumbsReact from './react/Breadcrumbs';
-import DataLoadingReact from './react/DataLoading';
-import ModalReact from './react/Modal';
-import MultiBodyReact from './react/MultiBody';
-import Redux from './react/Redux';
-import TransitionsReact from './react/Transitions';
+import ActiveLinksReact from "./react/ActiveLinks";
+import AuthenticationReact from "./react/Authentication";
+import BasicReact from "./react/Basic";
+import BlockingNavigationReact from "./react/BlockingNavigation";
+import BreadcrumbsReact from "./react/Breadcrumbs";
+import DataLoadingReact from "./react/DataLoading";
+import ModalReact from "./react/Modal";
+import MultiBodyReact from "./react/MultiBody";
+import MobX from "./react/MobX";
+import Redux from "./react/Redux";
+import TransitionsReact from "./react/Transitions";
 // vue
-import ActiveLinksVue from './vue/ActiveLinks';
-import AuthenticationVue from './vue/Authentication';
-import BasicVue from './vue/Basic';
-import BlockingNavigationVue from './vue/BlockingNavigation';
-import BreadcrumbsVue from './vue/Breadcrumbs';
-import TransitionsVue from './vue/Transitions';
+import ActiveLinksVue from "./vue/ActiveLinks";
+import AuthenticationVue from "./vue/Authentication";
+import BasicVue from "./vue/Basic";
+import BlockingNavigationVue from "./vue/BlockingNavigation";
+import BreadcrumbsVue from "./vue/Breadcrumbs";
+import TransitionsVue from "./vue/Transitions";
 // svelte
-import BasicSvelte from './svelte/Basic';
+import BasicSvelte from "./svelte/Basic";
 // full
-import TwitchFull from './full/Twitch';
+import TwitchFull from "./full/Twitch";
 
 export default {
   misc: {
-    'code-splitting': CodeSplitting,
-    'script-tags': ScriptTags,
-    'server-rendering': ServerRendering,
-    'side-effect': SideEffect
+    "code-splitting": CodeSplitting,
+    "script-tags": ScriptTags,
+    "server-rendering": ServerRendering,
+    "side-effect": SideEffect
   },
   react: {
-    'active-links': ActiveLinksReact,
+    "active-links": ActiveLinksReact,
     authentication: AuthenticationReact,
     basic: BasicReact,
-    'blocking-navigation': BlockingNavigationReact,
+    "blocking-navigation": BlockingNavigationReact,
     breadcrumbs: BreadcrumbsReact,
-    'data-loading': DataLoadingReact,
+    "data-loading": DataLoadingReact,
     modal: ModalReact,
-    'multi-body': MultiBodyReact,
+    "multi-body": MultiBodyReact,
+    mobx: MobX,
     redux: Redux,
     transitions: TransitionsReact
   },
@@ -49,10 +51,10 @@ export default {
     basic: BasicSvelte
   },
   vue: {
-    'active-links': ActiveLinksVue,
+    "active-links": ActiveLinksVue,
     authentication: AuthenticationVue,
     basic: BasicVue,
-    'blocking-navigation': BlockingNavigationVue,
+    "blocking-navigation": BlockingNavigationVue,
     breadcrumbs: BreadcrumbsVue,
     transitions: TransitionsVue
   },
--- documentation-website/src/client/constants/examples.js
+++ documentation-website/src/client/constants/examples.js
@@ -1,147 +1,153 @@
 const examples = {
   react: [
     {
-      name: 'Active Links',
-      category: 'react',
-      slug: 'active-links',
-      description: 'Style links when they match the current location'
+      name: "Active Links",
+      category: "react",
+      slug: "active-links",
+      description: "Style links when they match the current location"
     },
     {
-      name: 'Authentication',
-      category: 'react',
-      slug: 'authentication',
+      name: "Authentication",
+      category: "react",
+      slug: "authentication",
       description:
-        'Automatically redirect to a login page when attempting to access private content'
+        "Automatically redirect to a login page when attempting to access private content"
     },
     {
-      name: 'Basic',
-      category: 'react',
-      slug: 'basic',
-      description: 'A simple Curi app rendered using React'
+      name: "Basic",
+      category: "react",
+      slug: "basic",
+      description: "A simple Curi app rendered using React"
     },
     {
-      name: 'Blocking Navigation',
-      category: 'react',
-      slug: 'blocking-navigation',
-      description: 'Prevent navigation away from a half-filled form'
+      name: "Blocking Navigation",
+      category: "react",
+      slug: "blocking-navigation",
+      description: "Prevent navigation away from a half-filled form"
     },
     {
-      name: 'Breadcrumbs',
-      category: 'react',
-      slug: 'breadcrumbs',
-      description: 'Render breadcrumb links to ancestor routes'
+      name: "Breadcrumbs",
+      category: "react",
+      slug: "breadcrumbs",
+      description: "Render breadcrumb links to ancestor routes"
     },
     {
-      name: 'Data Loading',
-      category: 'react',
-      slug: 'data-loading',
-      description: 'Display a loading bar while waiting for data to load'
+      name: "Data Loading",
+      category: "react",
+      slug: "data-loading",
+      description: "Display a loading bar while waiting for data to load"
     },
     {
-      name: 'Modal Routes',
-      category: 'react',
-      slug: 'modal',
-      description: 'Load a route in a modal (the Pinterest model)'
+      name: "Modal Routes",
+      category: "react",
+      slug: "modal",
+      description: "Load a route in a modal (the Pinterest model)"
     },
     {
-      name: 'Multiple Body Components',
-      category: 'react',
-      slug: 'multi-body',
-      description: 'Attach multiple components to a route'
+      name: "Multiple Body Components",
+      category: "react",
+      slug: "multi-body",
+      description: "Attach multiple components to a route"
     },
     {
-      name: 'Redux',
-      category: 'react',
-      slug: 'redux',
-      description: 'Integrate Redux, React, and Curi (easily!)'
+      name: "MobX",
+      category: "react",
+      slug: "mobx",
+      description: "Integrate MobX, React, and Curi (easily!)"
     },
     {
-      name: 'Transitions',
-      category: 'react',
-      slug: 'transitions',
-      description: 'Transition between routes using react-transition-group'
+      name: "Redux",
+      category: "react",
+      slug: "redux",
+      description: "Integrate Redux, React, and Curi (easily!)"
+    },
+    {
+      name: "Transitions",
+      category: "react",
+      slug: "transitions",
+      description: "Transition between routes using react-transition-group"
     }
   ],
   vue: [
     {
-      name: 'Active Links',
-      category: 'vue',
-      slug: 'active-links',
-      description: 'Style links when they match the current location'
+      name: "Active Links",
+      category: "vue",
+      slug: "active-links",
+      description: "Style links when they match the current location"
     },
     {
-      name: 'Authentication',
-      category: 'vue',
-      slug: 'authentication',
+      name: "Authentication",
+      category: "vue",
+      slug: "authentication",
       description:
-        'Automatically redirect to a login page when attempting to access private content'
+        "Automatically redirect to a login page when attempting to access private content"
     },
     {
-      name: 'Basic',
-      category: 'vue',
-      slug: 'basic',
-      description: 'A simple Curi app rendered using VueJS'
+      name: "Basic",
+      category: "vue",
+      slug: "basic",
+      description: "A simple Curi app rendered using VueJS"
     },
     {
-      name: 'Breadcrumbs',
-      category: 'vue',
-      slug: 'breadcrumbs',
-      description: 'Render breadcrumb links to ancestor routes'
+      name: "Breadcrumbs",
+      category: "vue",
+      slug: "breadcrumbs",
+      description: "Render breadcrumb links to ancestor routes"
     },
     {
-      name: 'Blocking Navigation',
-      category: 'vue',
-      slug: 'blocking-navigation',
-      description: 'Prevent navigation away from a half-filled form'
+      name: "Blocking Navigation",
+      category: "vue",
+      slug: "blocking-navigation",
+      description: "Prevent navigation away from a half-filled form"
     },
     {
-      name: 'Transitions',
-      category: 'vue',
-      slug: 'transitions',
-      description: 'Transition between routes'
+      name: "Transitions",
+      category: "vue",
+      slug: "transitions",
+      description: "Transition between routes"
     }
   ],
   svelte: [
     {
-      name: 'Basic',
-      category: 'svelte',
-      slug: 'basic',
-      description: 'A simple Curi app rendered using Svelte'
+      name: "Basic",
+      category: "svelte",
+      slug: "basic",
+      description: "A simple Curi app rendered using Svelte"
     }
   ],
   full: [
     {
-      name: 'Twitch Clone',
-      category: 'full',
-      slug: 'twitch',
-      description: 'A clone of Twitch.tv built with Curi + Vue'
+      name: "Twitch Clone",
+      category: "full",
+      slug: "twitch",
+      description: "A clone of Twitch.tv built with Curi + Vue"
     }
   ],
   misc: [
     {
-      name: 'Code Splitting',
-      category: 'misc',
-      slug: 'code-splitting',
-      description: 'Use import() to enable Webpack code splitting'
+      name: "Code Splitting",
+      category: "misc",
+      slug: "code-splitting",
+      description: "Use import() to enable Webpack code splitting"
     },
     {
-      name: 'Script Tags',
-      category: 'misc',
-      slug: 'script-tags',
-      description: 'Load Curi packages using script tags instead of a bundle'
+      name: "Script Tags",
+      category: "misc",
+      slug: "script-tags",
+      description: "Load Curi packages using script tags instead of a bundle"
     },
     {
-      name: 'Server Rendering',
-      category: 'misc',
-      slug: 'server-rendering',
+      name: "Server Rendering",
+      category: "misc",
+      slug: "server-rendering",
       description:
-        'Render your application on the server using Node (this example uses Express)'
+        "Render your application on the server using Node (this example uses Express)"
     },
     {
-      name: 'Side Effects',
-      category: 'misc',
-      slug: 'side-effect',
-      description: 'Add side effects that always respond to navigation'
+      name: "Side Effects",
+      category: "misc",
+      slug: "side-effect",
+      description: "Add side effects that always respond to navigation"
     }
   ]
 };
