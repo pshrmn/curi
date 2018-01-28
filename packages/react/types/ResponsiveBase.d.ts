92566217e03b52d11c3441d53e676ae6510c0484
Paul Sherman <paul.sherman.88@gmail.com>
2018-01-27 22:33:09 -0600

    (examples) Update core/react deps [ci skip]

--- packages/react/types/ResponsiveBase.d.ts
+++ packages/react/types/ResponsiveBase.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="react" />
-import React from 'react';
-import { CuriRouter } from '@curi/core';
-import { CuriRenderFn } from './CuriBase';
+import React from "react";
+import { CuriRouter } from "@curi/core";
+import { CuriRenderFn } from "./CuriBase";
 export interface ResponsiveProps {
     router: CuriRouter;
     render: CuriRenderFn;
