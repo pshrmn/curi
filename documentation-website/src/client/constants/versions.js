2dca9d5b41fd6924d6221dd334710646f1c11ea5
Paul Sherman <paul.sherman.88@gmail.com>
2018-02-02 23:46:23 -0600

    Modify .prettierignores [ci skip]

--- documentation-website/src/client/constants/versions.js
+++ documentation-website/src/client/constants/versions.js
@@ -3,13 +3,13 @@ export default {
   "addon-active": "1.0.0-beta.5",
   "addon-ancestors": "1.0.0-beta.5",
   "addon-prefetch": "1.0.0-beta.6",
-  core: "1.0.0-beta.26",
-  mobx: "1.0.0-alpha.2",
-  react: "1.0.0-beta.20",
+  "core": "1.0.0-beta.26",
+  "mobx": "1.0.0-alpha.2",
+  "react": "1.0.0-beta.20",
   "react-native": "1.0.0-beta.1",
-  redux: "1.0.0-beta.4",
+  "redux": "1.0.0-beta.4",
   "side-effect-scroll": "1.0.0-beta.8",
   "side-effect-title": "1.0.0-beta.9",
-  svelte: "1.0.0-beta.4",
-  vue: "1.0.0-beta.16"
+  "svelte": "1.0.0-beta.4",
+  "vue": "1.0.0-beta.16"
 };
