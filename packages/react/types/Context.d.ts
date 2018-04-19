176b9716040694354eed503740ec7a05e6d66793
Paul Sherman <paul.sherman.88@gmail.com>
2018-04-18 22:38:27 -0500

    @curi/react types

--- packages/react/types/Context.d.ts
+++ packages/react/types/Context.d.ts
@@ -1,1 +1,5 @@
+/// <reference types="react" />
+import React from "react";
+import { Emitted } from "@curi/core";
+declare const Provider: React.ComponentType<React.ProviderProps<Emitted>>, Consumer: React.ComponentType<React.ConsumerProps<Emitted>>;
 export { Provider, Consumer as Curious };
