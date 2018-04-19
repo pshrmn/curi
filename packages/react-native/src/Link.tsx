a55eefaa6a167e29d33a015df181f8084803f84d
Paul Sherman <paul.sherman.88@gmail.com>
2018-04-19 10:36:54 -0500

    Don't need prop-types w/ new context

--- packages/react-native/src/Link.tsx
+++ packages/react-native/src/Link.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import invariant from "invariant";
 import { TouchableHighlight } from "react-native";
 import { Curious } from "@curi/react";
