4fcdb93e75bf8b95623d2519e6971ef2c13396b0
Paul Sherman <paul.sherman.88@gmail.com>
2018-04-21 10:40:51 -0500

    Move active functionality into <Active>

--- packages/react/src/Active.tsx
+++ packages/react/src/Active.tsx
@@ -15,31 +15,19 @@ export interface ActiveProps {
   partial?: boolean;
 }
 
-export interface BaseActiveProps extends ActiveProps {
-  router: CuriRouter;
-  response: Response;
-}
-
-function isActive(props: BaseActiveProps) {
-  const { partial = false, name, params, router, response } = props;
-  return router.route.active(name, response, params, partial);
-}
-
-const BaseActive = (props: BaseActiveProps) => {
-  invariant(
-    props.router.route.active,
-    'You are attempting to use the "active" prop, but have not included the "active" ' +
-      "route interaction (@curi/route-active) in your Curi router."
-  );
-
-  return props.children(isActive(props));
-};
-
 const Active = (props: ActiveProps): React.ReactElement<any> => (
   <Curious>
-    {({ router, response }) => (
-      <BaseActive {...props} router={router} response={response} />
-    )}
+    {({ router, response }) => {
+      invariant(
+        router.route.active,
+        'You are attempting to use the "active" prop, but have not included the "active" ' +
+          "route interaction (@curi/route-active) in your Curi router."
+      );
+
+      return props.children(
+        router.route.active(props.name, response, props.params, props.partial)
+      );
+    }}
   </Curious>
 );
 
--- packages/react/types/Active.d.ts
+++ packages/react/types/Active.d.ts
@@ -1,17 +1,12 @@
 /// <reference types="react" />
 import React from "react";
 import { ReactElement } from "react";
-import { CuriRouter, Response } from "@curi/core";
 export declare type ActiveChildren = (active: boolean) => ReactElement<any>;
 export interface ActiveProps {
     children: ActiveChildren;
     name: string;
     params?: object;
     partial?: boolean;
-}
-export interface BaseActiveProps extends ActiveProps {
-    router: CuriRouter;
-    response: Response;
 }
 declare const Active: (props: ActiveProps) => React.ReactElement<any>;
 export default Active;
