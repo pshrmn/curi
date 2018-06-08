88edc2a8608ffda8feecb4f764e0b37cc7a19bfe
Paul Sherman <paul.sherman.88@gmail.com>
2018-06-08 15:00:53 -0500

    Document @curi/side-effect-aria-live and use it [ci skip]

--- website/src/client/index.js
+++ website/src/client/index.js
@@ -3,21 +3,25 @@ import ReactDOM from "react-dom";
 import Browser from "@hickory/browser";
 import curi from "@curi/router";
 import { CuriProvider } from "@curi/react";
-import createTitleSideEffect from "@curi/side-effect-title";
-import createScrollSideEffect from "@curi/side-effect-scroll";
+import titleSideEffect from "@curi/side-effect-title";
+import scrollSideEffect from "@curi/side-effect-scroll";
+import ariaLiveSideEffect from "@curi/side-effect-aria-live";
 import active from "@curi/route-active";
 import prefetch from "@curi/route-prefetch";
 import routes from "./routes";
 import renderFunction from "./render";
 
-const setTitle = createTitleSideEffect({ suffix: "| Curi Documentation" });
-const scrollTo = createScrollSideEffect();
+const setTitle = titleSideEffect({ suffix: "| Curi Documentation" });
+const scrollTo = scrollSideEffect();
+const announce = ariaLiveSideEffect(
+  ({ response }) => `Navigated to ${response.title}`
+);
 
 const history = Browser();
 
 const router = curi(history, routes, {
   route: [active(), prefetch()],
-  sideEffects: [setTitle, scrollTo]
+  sideEffects: [setTitle, scrollTo, announce]
 });
 
 router.respond(() => {
