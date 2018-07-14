984b0121419b339c09a17b01b4c3b6c8415fe075
Paul Sherman <paul.sherman.88@gmail.com>
2018-07-14 13:49:42 -0500

    Keep @curi/core exports in line with @curi/router

--- packages/core/CHANGELOG.md
+++ packages/core/CHANGELOG.md
@@ -1,3 +1,7 @@
+## Next
+
+- Keep exports the same as new `@curi/router` exports.
+
 ## 1.0.0-beta.36
 
 - Moved functionality to `@curi/router`. This package now just re-exports `@curi/router`'s exported function.
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
-import curi from "@curi/router";
+import { curi, once, pathname } from "@curi/router";
 
 console.warn(
   'This package has been deprecated. You should switch to using "@curi/router" instead.'
 );
 
-export default curi;
+export { curi, once, pathname };
