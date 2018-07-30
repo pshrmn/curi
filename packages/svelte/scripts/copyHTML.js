77efeb9da58851b1d1aa671dbddcb21245a654fd
Paul Sherman <paul.sherman.88@gmail.com>
2018-07-30 11:55:38 -0500

    Fix Svelte build script for Node v10

--- packages/svelte/scripts/copyHTML.js
+++ packages/svelte/scripts/copyHTML.js
@@ -1,18 +1,23 @@
 const path = require("path");
 const glob = require("glob");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const BASE = path.resolve(__dirname, "..");
 const SRC = path.resolve(BASE, "src");
 const DIST = path.resolve(BASE, "dist");
 
 function copyHTML() {
-  fs.mkdir(DIST);
-  glob.glob(path.join(SRC, "*.html"), function(err, files) {
-    files.forEach(input => {
-      const name = path.basename(input);
-      const output = path.join(DIST, name);
-      fs.createReadStream(input).pipe(fs.createWriteStream(output));
+  fs.mkdir(DIST, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    glob.glob(path.join(SRC, "*.html"), function(err, files) {
+      files.forEach(input => {
+        const name = path.basename(input);
+        const output = path.join(DIST, name);
+        fs.createReadStream(input).pipe(fs.createWriteStream(output));
+      });
     });
   });
 }
