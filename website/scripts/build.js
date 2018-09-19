3187380d48b79940a84a365a9aaa92f3cb003a8c
Paul Sherman <paul.sherman.88@gmail.com>
2018-09-19 00:05:23 -0500

    Use generate & serve

--- website/scripts/build.js
+++ website/scripts/build.js
@@ -1,50 +1,33 @@
 require("@babel/register");
+
 const path = require("path");
-const generateStaticFiles = require("@curi/static");
+const React = require("react");
+const { generate, serve } = require("@curi/static");
+const active = require("@curi/route-active");
 
-const createApp = require("../src/server/app");
+const render = require("./render");
+const pages = require("./pages");
+const insert = require("./insert");
+
 const routes = require("../src/client/routes").default;
-const packages_api = require("../src/client/constants/packages").default;
-const guides_api = require("../src/client/constants/guides").default;
-const examples_api = require("../src/client/constants/examples").default;
-const tutorials_api = require("../src/client/constants/tutorials").default;
 
-const packageParams = packages_api.all().map(p => ({ package: p.name }));
-const guideParams = guides_api.all().map(p => ({ slug: p.slug }));
-const categories = examples_api.all();
-const exampleParams = Object.keys(categories)
-  .map(key => categories[key])
-  .reduce((acc, category) => {
-    const params = category.map(e => ({ category: e.category, slug: e.slug }));
-    acc = acc.concat(params);
-    return acc;
-  }, []);
-const tutorialParams = tutorials_api.all().map(t => ({ slug: t.slug }));
+const PORT = "9000";
+const OUTPUT_DIR = path.join(__dirname, "..", "gh-pages");
 
-let server;
-const app = createApp();
-server = app.listen("8000", () => {
-  generateStaticFiles(
-    routes,
-    [
-      { name: "Home" },
-
-      { name: "Packages" },
-      ...packageParams.map(params => ({ name: "Package", params })),
-
-      { name: "Guides" },
-      ...guideParams.map(params => ({ name: "Guide", params })),
-
-      { name: "Examples" },
-      ...exampleParams.map(params => ({ name: "Example", params })),
-
-      { name: "Tutorials" },
-      ...tutorialParams.map(params => ({ name: "Tutorial", params }))
-    ],
-    {
-      outputDir: path.join(__dirname, "..", "gh-pages")
-    }
-  ).then(() => {
-    server.close();
-  });
+serve({
+  port: PORT,
+  routes,
+  routerOptions: {
+    route: [active()]
+  },
+  doNotRenderRedirects: true,
+  render,
+  insert,
+  ready: () => {
+    return generate(routes, pages, {
+      port: PORT,
+      outputDir: OUTPUT_DIR,
+      outputRedirects: true
+    });
+  }
 });
---
+++ website/scripts/pages.js
@@ -0,0 +1,33 @@
+const routes = require("../src/client/routes").default;
+const packages_api = require("../src/client/constants/packages").default;
+const guides_api = require("../src/client/constants/guides").default;
+const examples_api = require("../src/client/constants/examples").default;
+const tutorials_api = require("../src/client/constants/tutorials").default;
+
+const packageParams = packages_api.all().map(p => ({ package: p.name }));
+const guideParams = guides_api.all().map(p => ({ slug: p.slug }));
+const categories = examples_api.all();
+const exampleParams = Object.keys(categories)
+  .map(key => categories[key])
+  .reduce((acc, category) => {
+    const params = category.map(e => ({ category: e.category, slug: e.slug }));
+    acc = acc.concat(params);
+    return acc;
+  }, []);
+const tutorialParams = tutorials_api.all().map(t => ({ slug: t.slug }));
+
+module.exports = [
+  { name: "Home" },
+
+  { name: "Packages" },
+  ...packageParams.map(params => ({ name: "Package", params })),
+
+  { name: "Guides" },
+  ...guideParams.map(params => ({ name: "Guide", params })),
+
+  { name: "Examples" },
+  ...exampleParams.map(params => ({ name: "Example", params })),
+
+  { name: "Tutorials" },
+  ...tutorialParams.map(params => ({ name: "Tutorial", params }))
+];
