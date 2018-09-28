7a26e5121479447a59c877ed5ec3b340174d36c3
Paul Sherman <paul.sherman.88@gmail.com>
2018-09-27 21:12:42 -0500

    (docs) pkg versions [ci skip]

--- website/src/constants/versions.js
+++ website/src/constants/versions.js
@@ -6,11 +6,11 @@ export default {
   "react-dom": "1.0.0",
   "react-native": "1.0.0",
   "react-universal": "1.0.0",
-  "router": "1.0.1",
+  "router": "1.0.2",
   "side-effect-aria-live": "1.0.0",
   "side-effect-scroll": "1.0.0",
   "side-effect-title": "1.0.0",
-  "static": "1.0.0-alpha.1",
+  "static": "1.0.0-alpha.2",
   "svelte": "1.0.0-beta.8",
   "vue": "1.0.0-beta.23"
 };
