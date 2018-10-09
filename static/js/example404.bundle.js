2e8615ac32157c91ca1e4f0ffcbe616475699646
Paul Sherman <paul.sherman.88@gmail.com>
2018-10-09 15:08:36 -0500

    Get rid of chunk hoist workaround

--- static/js/example404.bundle.js
+++ static/js/example404.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{112:function(e,n,t){"use strict";t.r(n),t.d(n,"default",function(){return r});var u=t(0),o=t.n(u);function r(){return o.a.createElement("div",null,"The requested example could not be found.")}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{112:function(e,n,t){"use strict";t.r(n),t.d(n,"default",function(){return r});var u=t(0),o=t.n(u);function r(){return o.a.createElement("div",null,"The requested example could not be found.")}}}]);
--- static/js/tutorial404.bundle.js
+++ static/js/tutorial404.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{107:function(n,e,t){"use strict";t.r(e),t.d(e,"default",function(){return r});var u=t(0),o=t.n(u);function r(){return o.a.createElement("div",null,"The requested tutorial could not be found.")}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{107:function(n,e,t){"use strict";t.r(e),t.d(e,"default",function(){return r});var u=t(0),o=t.n(u);function r(){return o.a.createElement("div",null,"The requested tutorial could not be found.")}}}]);
