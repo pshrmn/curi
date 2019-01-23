3f91c380e433bffa09f70f4f235c95fbdab17f05
Paul Sherman <paul.sherman.88@gmail.com>
2019-01-23 01:52:54 -0600

    Get rid of <code> overflows

--- static/js/tutorial.bundle.js
+++ static/js/tutorial.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{35:function(n,e,t){"use strict";t.r(e),t.d(e,"default",function(){return c});var a=t(0),r=t.n(a),u=t(21);function c(n){var e=n.response.data.content;return r.a.createElement(u.a,null,r.a.createElement(e,null))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[76],{32:function(n,e,t){"use strict";t.r(e),t.d(e,"default",function(){return c});var a=t(0),r=t.n(a),u=t(18);function c(n){var e=n.response.data.content;return r.a.createElement(u.a,null,r.a.createElement(e,null))}}}]);
