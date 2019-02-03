cbf34153afdb89035c6f84e40a59c98a6247568b
Paul Sherman <paul.sherman.88@gmail.com>
2019-02-03 17:14:36 -0600

    (docs) Fix build [ci skip]

--- website/scripts/render.js
+++ website/scripts/render.js
@@ -2,12 +2,14 @@ const React = require("react");
 const { renderToString } = require("react-dom/server");
 const { curiProvider } = require("@curi/react-dom");
 
-const renderFn = require("../src/render").default;
+const App = require("../src/components/App").default;
 
 module.exports = function render(emitted) {
   const Router = curiProvider(emitted.router);
   return {
-    html: renderToString(React.createElement(Router, null, renderFn)),
+    html: renderToString(
+      React.createElement(Router, null, React.createElement(App))
+    ),
     title: emitted.response.title
   };
 };
