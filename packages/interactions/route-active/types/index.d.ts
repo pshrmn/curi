801b90189f6d189825636cbd0d8fa4e582623e34
Paul Sherman <paul.sherman.88@gmail.com>
2019-02-03 23:32:46 -0600

    @curi/route-active move optional args to opts obj

--- packages/interactions/route-active/types/index.d.ts
+++ packages/interactions/route-active/types/index.d.ts
@@ -1,2 +1,9 @@
-import { Interaction } from "@curi/router";
+import { Interaction, Params } from "@curi/router";
+import { HickoryLocation } from "@hickory/root";
+export declare type LocationCheck = (l: HickoryLocation) => boolean;
+export interface ActiveCheckOptions {
+    params?: Params;
+    partial?: boolean;
+    locationCheck?: LocationCheck;
+}
 export default function checkIfActive(): Interaction;
