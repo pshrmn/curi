34e8d636fb3fad7fe322ac97241a41629eefdc97
Paul Sherman <paul.sherman.88@gmail.com>
2019-02-18 23:24:33 -0600

    Split React Link components into Link/NavLink (#163)

--- packages/react-dom/src/Link.tsx
+++ packages/react-dom/src/Link.tsx
@@ -1,32 +1,60 @@
 import React from "react";
-import { useHref, useNavigationHandler } from "@curi/react-universal";
+import {
+  useHref,
+  useNavigationHandler,
+  useStatefulNavigationHandler
+} from "@curi/react-universal";
 import { canNavigate } from "./utils";
 
 import { RouteLocation } from "@curi/router";
 import { NavigatingChildren } from "@curi/react-universal";
 
-export interface LinkProps extends RouteLocation {
+export interface BaseLinkProps extends RouteLocation {
   onNav?: (e: React.MouseEvent<HTMLElement>) => void;
   anchor?: React.ReactType;
-  children: NavigatingChildren | React.ReactNode;
   forward?: React.AnchorHTMLAttributes<HTMLAnchorElement>;
 }
 
-const HookLink = React.forwardRef((props: LinkProps, ref: React.Ref<any>) => {
-  const href = useHref(props);
+export interface LinkProps extends BaseLinkProps {
+  children: React.ReactNode;
+}
 
-  const { eventHandler, children } = useNavigationHandler<
-    React.MouseEvent<HTMLElement>
-  >(props, canNavigate);
+export interface NavLinkProps extends BaseLinkProps {
+  children: NavigatingChildren;
+}
 
-  const { anchor: Anchor = "a", forward } = props;
+export const Link = React.forwardRef(
+  (props: LinkProps, ref: React.Ref<any>) => {
+    const href = useHref(props);
 
-  return (
-    // @ts-ignore
-    <Anchor onClick={eventHandler} href={href} ref={ref} {...forward}>
-      {children}
-    </Anchor>
-  );
-});
+    const { eventHandler } = useNavigationHandler<
+      React.MouseEvent<HTMLElement>
+    >(props, canNavigate);
 
-export default HookLink;
+    const { anchor: Anchor = "a", forward, children } = props;
+
+    return (
+      <Anchor onClick={eventHandler} href={href} ref={ref} {...forward}>
+        {children}
+      </Anchor>
+    );
+  }
+);
+
+export const NavLink = React.forwardRef(
+  (props: NavLinkProps, ref: React.Ref<any>) => {
+    const href = useHref(props);
+
+    const { eventHandler, navigating } = useStatefulNavigationHandler<
+      React.MouseEvent<HTMLElement>
+    >(props, canNavigate);
+
+    const { anchor: Anchor = "a", forward, children } = props;
+
+    return (
+      <Anchor onClick={eventHandler} href={href} ref={ref} {...forward}>
+        {children(navigating)}
+      </Anchor>
+    );
+  }
+);
--- packages/react-dom/types/index.d.ts
+++ packages/react-dom/types/index.d.ts
@@ -1,6 +1,6 @@
-export { LinkProps } from "./Link";
+export { LinkProps, NavLinkProps } from "./Link";
 export { FocusHookProps } from "./hooks/useNavigationFocus";
 import useNavigationFocus from "./hooks/useNavigationFocus";
-import Link from "./Link";
+import { Link, NavLink } from "./Link";
 export * from "@curi/react-universal";
-export { Link, useNavigationFocus };
+export { Link, NavLink, useNavigationFocus };
