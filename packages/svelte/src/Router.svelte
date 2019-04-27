10be25e4c2b167f36c23efe2b5850675f4784496
Paul Sherman <paul.sherman.88@gmail.com>
2019-04-26 23:16:22 -0500

    Router sets navigation

--- packages/svelte/src/Router.svelte
+++ packages/svelte/src/Router.svelte
@@ -1,10 +1,11 @@
 <script>
-  import { setRouter, setResponse } from "@curi/svelte";
+  import { setRouter, setResponse, setNavigation } from "@curi/svelte";
 
   export let stores;
 
   setRouter(stores.router);
   setResponse(stores.response);
+  setNavigation(stores.navigation);
 </script>
 
 <slot></slot>
